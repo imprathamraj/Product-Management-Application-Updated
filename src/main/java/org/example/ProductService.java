09ae369a6582ee9433379d955c637028533395cb
imprathamraj <rjpratham05@gmail.com>
2025-03-23 17:10:53 +0530

    [Pratham Raj] Add. productDB object of ProductDB class and modified all methods accordingly

--- src/main/java/org/example/ProductService.java
+++ src/main/java/org/example/ProductService.java
@@ -6,69 +6,40 @@ import java.util.List;
 public class ProductService {
 
     List<Product> products = new ArrayList<>();
-
+    ProductDB productDB = new ProductDB();
     // Adding product
     public void addProduct(Product product) {
         products.add(product);
+        productDB.save(product);
     }
 
     // Getting all product
     public List<Product> getAllProduct() {
-        return products;
+        return productDB.getAll();
     }
 
     // Getting product with a particular name
     public Product getProductByName(String name){
-        for(Product p: products){
-            if(p.getName().equals(name)){
-                return p;
-            }
-        }
-        return null;
+        return productDB.getProductByName(name);
     }
 
     // Getting product with a particular place
     public List<Product> getProductByPlace(String place){
-        List<Product> productList = new ArrayList<>();
-        for(Product p: products){
-            if(p.getPlace().equals(place)){
-                productList.add(p);
-            }
-        }
-        return productList;
+        return productDB.getProductByPlace(place);
     }
 
     // Getting product with a particular type
     public List<Product> getProductByType(String type){
-        List<Product> productList = new ArrayList<>();
-        for(Product p: products){
-            if(p.getType().equals(type)){
-                productList.add(p);
-            }
-        }
-        return productList;
+        return productDB.getProductByType(type);
     }
 
     // Getting product with expired warranty
     public List<Product> getProductByExpiredWarranty() {
-        List<Product> productList = new ArrayList<>();
-        for(Product p: products){
-            if(p.getWarranty()<2025){
-                productList.add(p);
-            }
-        }
-        return productList;
+        return productDB.getProductByExpiredWarranty();
     }
 
     // Getting product with particular text
     public List<Product> getProductByText(String text) {
-        String str = text.toLowerCase();
-        List<Product> productList = new ArrayList<>();
-        for(Product p: products){
-            if(p.getName().toLowerCase().contains(str) || p.getType().toLowerCase().contains(str) || p.getPlace().toLowerCase().contains(str)){
-                productList.add(p);
-            }
-        }
-        return productList;
+        return productDB.getProductByText(text);
     }
 }
